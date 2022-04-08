49f10df47a8980ebdfce299d86aec28150615311
saijal21 <44276241+saijal21@users.noreply.github.com>
2022-04-09 00:56:07 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,12 +4,8 @@ Created on Fri Apr  8 17:50:45 2022
 
 @author: SAIJAL
 """
-import os 
-os.chdir("D:\\MAGDEBURG STUDIES\\WINTER_SEM_2020\\Thesis\\High_Level_Descriptors\\Evaluate_HLD_Clusters\\UI_MAKING\\Dashboard_Final")
+
 from explainerdashboard import ExplainerDashboard
-
-
-
 db = ExplainerDashboard.from_config("dashboard.yaml")
 app = db.flask_server()
 
